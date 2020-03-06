0f17c82477c9c6ca21f6dbb73e53f7751d14e533
Jiwon <jiwon1.han@gmail.com>
2020-03-06 12:26:36 -0800

    Add methods

--- WordCounter/Models/Player.cs
+++ WordCounter/Models/Player.cs
@@ -5,6 +5,8 @@ namespace WordCounter.Models
     public class Player
     {
         public static string Name {get; set;}
+        public static string Word {get; set;}
+        public static string Sentence {get; set;}
 
         public Player(string name)
         {
